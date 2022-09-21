599975cb10110a1db503d00e6300a0a6f47171cf
İsmail Öksüz <46068969+ismailoksuz22@users.noreply.github.com>
2022-09-22 01:00:43 +0300

    Add files via upload

---
+++ Problem Solving/Extra Long Factorials.cs
@@ -0,0 +1,39 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+using System.Numerics;
+class Result
+{
+
+
+    public static void extraLongFactorials(int n)
+    {
+        var res=BigInteger.One;
+        while(n>0){
+            res*=n;
+            n-=1;
+        }
+        Console.Write(res);
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        Result.extraLongFactorials(n);
+    }
+}
