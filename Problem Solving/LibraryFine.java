7d88ee0b2c15c2639ed67e063bbac805a4d5958f
İsmail Öksüz <46068969+ismailoksuz@users.noreply.github.com>
2022-10-21 01:11:21 +0300

    Add files via upload

---
+++ Problem Solving/LibraryFine.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+
+    public static int libraryFine(int d1, int m1, int y1, int d2, int m2, int y2) {
+        int y=(y1-y2)*10000;
+        int m=(m1-m2)*500;
+        int d=(d1-d2)*15;
+        if(y>0)return Math.abs(y);
+        else if(y==0 && m>0)return Math.abs(m);
+        else if(y==0 && m==0 && d>0)return Math.abs(d);
+        else return 0;
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int d1 = Integer.parseInt(firstMultipleInput[0]);
+
+        int m1 = Integer.parseInt(firstMultipleInput[1]);
+
+        int y1 = Integer.parseInt(firstMultipleInput[2]);
+
+        String[] secondMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int d2 = Integer.parseInt(secondMultipleInput[0]);
+
+        int m2 = Integer.parseInt(secondMultipleInput[1]);
+
+        int y2 = Integer.parseInt(secondMultipleInput[2]);
+
+        int result = Result.libraryFine(d1, m1, y1, d2, m2, y2);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
